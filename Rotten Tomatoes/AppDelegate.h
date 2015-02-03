1c4c1b016797eb566f6e455e8268441584cb4d0c
Calvin Tuong <ctuong@box.com>
2015-02-03 00:22:05 -0800

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Calvin Tuong on 2/3/15.
+//  Copyright (c) 2015 Calvin Tuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
